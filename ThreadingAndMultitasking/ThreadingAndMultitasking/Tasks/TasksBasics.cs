acd556cc5dfaa302e2b56c53210795b3d73b9d76
Jakub Marcickiewicz <jakub.marcickiewicz@plk-sa.pl>
2018-01-09 16:18:11 +0100

    Some task example

--- ThreadingAndMultitasking/ThreadingAndMultitasking/Tasks/TasksBasics.cs
+++ ThreadingAndMultitasking/ThreadingAndMultitasking/Tasks/TasksBasics.cs
@@ -16,6 +16,28 @@ namespace ThreadingAndMultitasking.Tasks
                 }
             });
             t.Wait();
+
+            Task<int> t1 = Task.Run(() =>
+            {
+                return 43;
+            });
+
+            Console.WriteLine("\nThis sample returns a value from Task<int>");
+            Console.WriteLine($"Result from current Task<int> = {t1.Result}");
+
+            Task<int> t2 = Task.Run(() =>
+            {
+                return 43;
+            }).ContinueWith((task) =>
+            {
+                return task.Result * 2;
+            });
+
+            Console.WriteLine("\nThis sample returns a value from Task<int> with Continuation Task");
+            Console.WriteLine($"Result from current Task<int> (result * 2) = {t2.Result}");
+            
+            Console.WriteLine("");
+            Console.WriteLine("");
         }
     }
 }
