55c722a0959bb3aeee3e17e3ee448526076e9092
Jakub Marcickiewicz <marcickiewiczjakub@gmail.com>
2018-02-19 21:37:57 +0100

    A litle code with reflection

---
+++ Chapter 2/2.5/ReflectionTests/UsingReflectionOne.cs
@@ -0,0 +1,40 @@
+﻿using HelpersLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionTests
+{
+    public interface IPlugin
+    {
+        string Name { get; }
+        string Description { get; }
+        bool Load(MyApplication myApplication);
+    }
+
+    class UsingReflectionOne : IRun
+    {
+        public void Run()
+        {
+
+        }
+    }
+
+    class MyPlugin : IPlugin
+    {
+        public string Name { get { return "MyPlugin"; } }
+
+        public string Description { get { return "My Sample Plugin"; } }
+
+        public bool Load(MyApplication myApplication)
+        {
+            return true;
+        }
+    }
+
+    public class MyApplication
+    {
+    }
+}
