e7d0f519f2aaf0516c1f507ba096d406b85917de
Jakub Marcickiewicz <marcickiewiczjakub@gmail.com>
2018-03-28 11:04:54 +0200

    starting module 3

---
+++ Chapter 3/3.1/ValidateApplicationInputTests/ParteTryParseConvertTests.cs
@@ -0,0 +1,22 @@
+﻿using HelpersLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidateApplicationInputTests
+{
+    public class ParteTryParseConvertTests : IRun
+    {
+        public void Run()
+        {
+
+        }
+
+        private void UsingParse()
+        {
+
+        }
+    }
+}
